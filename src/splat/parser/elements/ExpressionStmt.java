e8d349e126115a4d316af808c4ff6a85d074972d
aigerim <74128426+aigerim-omirzak@users.noreply.github.com>
2025-11-28 16:58:25 +0500

    Refactor default value utilities

--- src/splat/parser/elements/ExpressionStmt.java
+++ src/splat/parser/elements/ExpressionStmt.java
@@ -1,5 +1,13 @@
 package splat.parser.elements;
 
+import java.util.Map;
+
+import splat.executor.ExecutionException;
+import splat.executor.ReturnFromCall;
+import splat.executor.Value;
+import splat.semanticanalyzer.SemanticAnalysisException;
+import splat.semanticanalyzer.Type;
+
 public class ExpressionStmt extends Statement {
     private Expression expr;
 
@@ -10,5 +18,17 @@ public class ExpressionStmt extends Statement {
 
     public Expression getExpression() {
         return expr;
+    }
+
+    @Override
+    public void analyze(Map<String, FunctionDecl> funcMap, Map<String, Type> varAndParamMap)
+            throws SemanticAnalysisException {
+        expr.analyzeAndGetType(funcMap, varAndParamMap);
+    }
+
+    @Override
+    public void execute(Map<String, FunctionDecl> funcMap, Map<String, Value> varAndParamMap)
+            throws ReturnFromCall, ExecutionException {
+        expr.evaluate(funcMap, varAndParamMap);
     }
 }
--- src/splat/parser/elements/FunctionCallStmt.java
+++ src/splat/parser/elements/FunctionCallStmt.java
@@ -1,6 +1,13 @@
 package splat.parser.elements;
 
+import java.util.Map;
+
+import splat.executor.ExecutionException;
+import splat.executor.ReturnFromCall;
+import splat.executor.Value;
 import splat.lexer.Token;
+import splat.semanticanalyzer.SemanticAnalysisException;
+import splat.semanticanalyzer.Type;
 
 public class FunctionCallStmt extends Statement {
     private final FunctionCall call;
@@ -18,6 +25,18 @@ public class FunctionCallStmt extends Statement {
 
     public Token getStartToken() {
         return call.getStartToken();
+    }
+
+    @Override
+    public void analyze(Map<String, FunctionDecl> funcMap, Map<String, Type> varAndParamMap)
+            throws SemanticAnalysisException {
+        call.analyzeAndGetType(funcMap, varAndParamMap);
+    }
+
+    @Override
+    public void execute(Map<String, FunctionDecl> funcMap, Map<String, Value> varAndParamMap)
+            throws ReturnFromCall, ExecutionException {
+        call.evaluate(funcMap, varAndParamMap);
     }
 
 
