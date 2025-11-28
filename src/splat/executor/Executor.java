c275d30efdb49bf2d68cdb0a8e219f3a7205ac69
aigerim <74128426+aigerim-omirzak@users.noreply.github.com>
2025-11-28 19:05:28 +0500

    Fix printing and propagate global state from function calls

---
+++ src/splat/executor/Executor.java
@@ -0,0 +1,125 @@
+package splat.executor;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import splat.parser.elements.Declaration;
+import splat.parser.elements.FunctionCall;
+import splat.parser.elements.FunctionDecl;
+import splat.parser.elements.ProgramAST;
+import splat.parser.elements.Statement;
+import splat.parser.elements.VariableDecl;
+import splat.semanticanalyzer.SemanticAnalysisException;
+import splat.semanticanalyzer.Type;
+
+public class Executor {
+
+    private ProgramAST progAST;
+
+    private Map<String, FunctionDecl> funcMap;
+    private Map<String, Value> progVarMap;
+
+    public Executor(ProgramAST progAST) {
+        this.progAST = progAST;
+    }
+
+    public void runProgram() throws ExecutionException {
+
+        setMaps();
+
+        try {
+            for (Statement stmt : progAST.getStmts()) {
+                stmt.execute(funcMap, progVarMap);
+            }
+        } catch (ReturnFromCall rfc) {
+            throw new ExecutionException("Return cannot be used outside of a function", 0, 0);
+        }
+    }
+
+    private void setMaps() {
+
+        funcMap = new HashMap<>();
+        progVarMap = new HashMap<>();
+
+        for (Declaration decl : progAST.getDecls()) {
+
+            String label = decl.getLabelLexeme();
+
+            if (decl instanceof FunctionDecl) {
+                FunctionDecl funcDecl = (FunctionDecl)decl;
+                funcMap.put(label, funcDecl);
+
+            } else if (decl instanceof VariableDecl) {
+                VariableDecl varDecl = (VariableDecl)decl;
+                try {
+                    Type type = Type.fromToken(varDecl.getType());
+                    progVarMap.put(label, Value.defaultFor(type));
+                } catch (SemanticAnalysisException sae) {
+                    // Should have been caught earlier during analysis
+                    throw new RuntimeException(sae);
+                }
+            }
+        }
+    }
+
+    public Value callFunction(FunctionCall call, Map<String, Value> callerVarMap)
+            throws ExecutionException, ReturnFromCall {
+
+        FunctionDecl decl = funcMap.get(call.getName().getLexeme());
+        if (decl == null) {
+            throw new ExecutionException("Unknown function '" + call.getName().getLexeme() + "'",
+                    call.getLine(), call.getColumn());
+        }
+
+        List<VariableDecl> params = decl.getParams();
+        if (params.size() != call.getArgs().size()) {
+            throw new ExecutionException("Incorrect number of arguments for function", call.getLine(), call.getColumn());
+        }
+
+        Map<String, Value> newVarMap = new HashMap<>();
+        // Include program-level variables for read/write access
+        newVarMap.putAll(progVarMap);
+
+        for (int i = 0; i < params.size(); i++) {
+            VariableDecl param = params.get(i);
+            Value argVal = call.getArgs().get(i).evaluate(funcMap, callerVarMap);
+            newVarMap.put(param.getLabelLexeme(), argVal);
+        }
+
+        for (VariableDecl local : decl.getLocalVars()) {
+            try {
+                Type type = Type.fromToken(local.getType());
+                newVarMap.put(local.getLabelLexeme(), Value.defaultFor(type));
+            } catch (SemanticAnalysisException sae) {
+                throw new ExecutionException(sae.getMessage(), local.getLine(), local.getColumn());
+            }
+        }
+
+        try {
+            for (Statement stmt : decl.getStmts()) {
+                stmt.execute(funcMap, newVarMap);
+            }
+        } catch (ReturnFromCall rfc) {
+            syncGlobals(progVarMap, newVarMap);
+            return rfc.getValue();
+        }
+
+        syncGlobals(progVarMap, newVarMap);
+
+        try {
+            return Value.defaultFor(Type.fromToken(decl.getReturnType()));
+        } catch (SemanticAnalysisException sae) {
+            throw new ExecutionException(sae.getMessage(), decl.getLine(), decl.getColumn());
+        }
+    }
+
+    private void syncGlobals(Map<String, Value> globals, Map<String, Value> calleeMap) {
+        for (String label : globals.keySet()) {
+            if (calleeMap.containsKey(label)) {
+                globals.put(label, calleeMap.get(label));
+            }
+        }
+    }
+}
+
--- src/splat/parser/elements/WhileLoop.java
+++ src/splat/parser/elements/WhileLoop.java
@@ -2,6 +2,13 @@ package splat.parser.elements;
 
 import splat.lexer.Token;
 import java.util.List;
+import java.util.Map;
+
+import splat.executor.ExecutionException;
+import splat.executor.ReturnFromCall;
+import splat.executor.Value;
+import splat.semanticanalyzer.SemanticAnalysisException;
+import splat.semanticanalyzer.Type;
 
 public class WhileLoop extends Statement {
     private Expression condition;
@@ -21,4 +28,25 @@ public class WhileLoop extends Statement {
 
     public Expression getCondition() { return condition; }
     public List<Statement> getBody() { return body; }
+
+    @Override
+    public void analyze(Map<String, FunctionDecl> funcMap, Map<String, Type> varAndParamMap)
+            throws SemanticAnalysisException {
+        if (condition.analyzeAndGetType(funcMap, varAndParamMap) != Type.BOOLEAN) {
+            throw new SemanticAnalysisException("While condition must be boolean", whileToken.getLine(), whileToken.getCol());
+        }
+        for (Statement stmt : body) {
+            stmt.analyze(funcMap, varAndParamMap);
+        }
+    }
+
+    @Override
+    public void execute(Map<String, FunctionDecl> funcMap, Map<String, Value> varAndParamMap)
+            throws ReturnFromCall, ExecutionException {
+        while (condition.evaluate(funcMap, varAndParamMap).asBoolean()) {
+            for (Statement stmt : body) {
+                stmt.execute(funcMap, varAndParamMap);
+            }
+        }
+    }
 }
