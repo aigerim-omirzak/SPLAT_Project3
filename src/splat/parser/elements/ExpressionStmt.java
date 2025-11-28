f607c927ef608da0fec02c5a857eb05246ba63fd
aigerim <74128426+aigerim-omirzak@users.noreply.github.com>
2025-11-28 18:11:46 +0500

    Clean up statement wrappers

--- src/splat/parser/elements/ExpressionStmt.java
+++ src/splat/parser/elements/ExpressionStmt.java
@@ -1,14 +1,34 @@
 package splat.parser.elements;
 
+import java.util.Map;
+
+import splat.executor.ExecutionException;
+import splat.executor.ReturnFromCall;
+import splat.executor.Value;
+import splat.semanticanalyzer.SemanticAnalysisException;
+import splat.semanticanalyzer.Type;
+
 public class ExpressionStmt extends Statement {
-    private Expression expr;
+    private final Expression expr;
 
     public ExpressionStmt(Expression expr) {
-        super(expr.getToken());
+        super(expr.getStartToken());
         this.expr = expr;
     }
 
     public Expression getExpression() {
         return expr;
+    }
+
+    @Override
+    public void analyze(Map<String, FunctionDecl> funcMap, Map<String, Type> varAndParamMap)
+            throws SemanticAnalysisException {
+        expr.analyzeAndGetType(funcMap, varAndParamMap);
+    }
+
+    @Override
+    public void execute(Map<String, FunctionDecl> funcMap, Map<String, Value> varAndParamMap)
+            throws ReturnFromCall, ExecutionException {
+        expr.evaluate(funcMap, varAndParamMap);
     }
 }
--- src/splat/parser/elements/PrintStmt.java
+++ src/splat/parser/elements/PrintStmt.java
@@ -1,18 +1,38 @@
 package splat.parser.elements;
 
+import java.util.Map;
+
+import splat.executor.ExecutionException;
+import splat.executor.ReturnFromCall;
+import splat.executor.Value;
 import splat.lexer.Token;
+import splat.semanticanalyzer.SemanticAnalysisException;
+import splat.semanticanalyzer.Type;
 
 public class PrintStmt extends Statement {
-    private Expression expr;
+    private final Expression expr;
 
     public PrintStmt(Token tok, Expression expr) {
         super(tok);
         this.expr = expr;
     }
 
+    @Override
+    public void analyze(Map<String, FunctionDecl> funcMap, Map<String, Type> varAndParamMap)
+            throws SemanticAnalysisException {
+        if (expr != null) {
+            expr.analyzeAndGetType(funcMap, varAndParamMap);
+        }
+    }
 
-    public Token getStartToken() {
-        return super.getStartToken();
+    @Override
+    public void execute(Map<String, FunctionDecl> funcMap, Map<String, Value> varAndParamMap)
+            throws ReturnFromCall, ExecutionException {
+        if (expr != null) {
+            Value v = expr.evaluate(funcMap, varAndParamMap);
+            System.out.print(v.getRaw());
+        }
+        System.out.println();
     }
 
     @Override
